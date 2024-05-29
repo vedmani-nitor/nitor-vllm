ed395e15b7c1219bc927783542fb0152b30f286c
vedmani-nitor <vedmani.vaidya@nitorinfotech.com>
2024-05-29 19:54:34 +0530

    Update vllm_ui_multiuser.py

--- vllm_ui_multiuser.py
+++ vllm_ui_multiuser.py
@@ -28,49 +28,11 @@ dtype = args.dtype
 model_name = "microsoft/Phi-3-mini-4k-instruct"
 
 api_key = "token-abc123"
-
-command = f"nohup python -m vllm.entrypoints.openai.api_server --model {model_name} --dtype {dtype} --api-key {api_key} --trust-remote-code &"
+print("Launching the server")
+command = f"python -m vllm.entrypoints.openai.api_server --model {model_name} --dtype {dtype} --api-key {api_key} --trust-remote-code"
 
 # Use shell=True only if the command is simple and doesn't contain special characters
-process = subprocess.Popen(
-    command, shell=True, stdout=open("my_logfile.log", "a"), stderr=subprocess.PIPE
-)
-
-pid = process.pid
-# print(f"Process started with PID: {pid}")
-
-
-print("Waiting for the server to start..\r", end="")
-time.sleep(120)
-print("")
-
-
-client = OpenAI(
-    base_url="http://localhost:8000/v1",
-    api_key="token-abc123",
-)
-
-"""Need to check how history is getting handled after concurrent use"""
-
-print("Openai Client created")
-
-
-def predict(message, history):
-    history_openai_format = []
-    for human, assistant in history:
-        history_openai_format.append({"role": "user", "content": human})
-        history_openai_format.append({"role": "assistant", "content": assistant})
-    history_openai_format.append({"role": "user", "content": message})
-
-    response = client.chat.completions.create(
-        model=model_name, messages=history_openai_format, temperature=1.0, stream=True
-    )
-
-    partial_message = ""
-    for chunk in response:
-        if chunk.choices[0].delta.content is not None:
-            partial_message = partial_message + chunk.choices[0].delta.content
-            yield partial_message
-
-
-gr.ChatInterface(predict).queue(default_concurrency_limit=5).launch()
+# process = subprocess.Popen(
+#     command, shell=True, stdout=open("my_logfile.log", "a"), stderr=subprocess.PIPE
+# )
+os.system(command)
